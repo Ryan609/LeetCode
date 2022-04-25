3d3d2400ad1b96626dad82b7b7c9ce8ec011d2bc
Xinyan <8380@88.com>
2022-04-25 20:17:48 +0930

    files

--- LinkedList/List_141.java
+++ LinkedList/List_141.java
@@ -19,10 +19,13 @@ public class List_141 {
     public boolean hasCycle(ListNode head) {
         ListNode fast = head;
         ListNode low = head;
-        fast = fast.next.next;
-        low = low.next;
-        if (fast == low) {
-            return true;
+        
+        while (fast != null && fast.next != null) {
+            fast = fast.next.next;
+            low = low.next;
+            if (fast == low) {
+                return true;
+            }
         }
         return false;
     }
